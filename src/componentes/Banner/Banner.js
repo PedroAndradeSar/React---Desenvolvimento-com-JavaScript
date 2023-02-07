adf177e1fcec02cffce0e8c28a38693313c4c66d
Pedro Henrique Andrade Sartorato <phandradesar@gmail.com>
2023-02-07 13:53:42 -0300

    Aula - Campo de Texto

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css'; //importação do css.
 
-function Banner() {
+export const Banner = () => {
     return (
         //não pode usar a class apenas, pois é uma palavra reservada do js, teria que usar o className=""
         <header className="banner"> 
@@ -9,5 +9,3 @@ function Banner() {
         
     )
 };
-
-export default Banner;
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,16 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return(
+        <div className="campo-texto">
+            <label>
+                Nome
+            </label>
+            <input placeholder='Digite seu Nome'/>
+              
+
+        </div>
+    ) 
+};
+
+export default CampoTexto;
